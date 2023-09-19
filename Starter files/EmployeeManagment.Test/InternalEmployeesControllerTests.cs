ee373abf88dc733dc0d7cd778cea9e383ff7aada
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2023-09-19 22:53:50 +0300

    Half of section 8

---
+++ Starter files/EmployeeManagment.Test/InternalEmployeesControllerTests.cs
@@ -0,0 +1,66 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.Controllers;
+using EmployeeManagement.DataAccess.Entities;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace EmployeeManagment.Test;
+public class InternalEmployeesControllerTests
+{
+    private readonly InternalEmployeesController _internalEmployeesController;
+
+    public InternalEmployeesControllerTests()
+    {
+        var employeeServiceMock = new Mock<IEmployeeService>();
+        employeeServiceMock.Setup(m => m.FetchInternalEmployeesAsync())
+            .ReturnsAsync(new List<InternalEmployee>()
+            {
+                new InternalEmployee("Megan", "Jones", 2, 3000, false, 2),
+                new InternalEmployee("Jaimy", "Johnson", 3, 3400, true, 1),
+                new InternalEmployee("Anne", "Adams", 3, 4000, false, 3)
+            });
+
+        _internalEmployeesController = new InternalEmployeesController(
+           employeeServiceMock.Object, null);
+    }
+
+    [Fact]
+    public async Task GetInternalEmployees_GetAction_MustReturnOkObjectResult()
+    {
+        // AAA
+        // Arrange
+        // Act
+        var result = await _internalEmployeesController.GetInternalEmployees();
+
+        // Assert
+        Assert.IsType<OkObjectResult>(result.Result);
+    }
+
+    [Fact]
+    public async Task GetInternalEmployees_GetAction_MustReturnIEnumerableOfInternalEmployeeDtoAsModelType()
+    {
+        // Arrange
+        // Act
+        var result = await _internalEmployeesController.GetInternalEmployees();
+
+        var actionResult = (result.Result as OkObjectResult).Value;
+
+        // Assert
+        Assert.IsAssignableFrom<IEnumerable<InternalEmployeeDto>>(actionResult);
+    }
+
+    [Fact]
+    public async Task GetInternalEmployees_GetAction_MustReturnNumberOfInputtedInternalEmployees()
+    {
+        // Arrange
+        // Act
+        var result = await _internalEmployeesController.GetInternalEmployees();
+
+        var employeesDtos = ((result.Result as OkObjectResult).Value);
+
+        // Assert
+        Assert.Equal(3, (employeesDtos as IEnumerable<InternalEmployeeDto>).Count());
+    }
+
+}
