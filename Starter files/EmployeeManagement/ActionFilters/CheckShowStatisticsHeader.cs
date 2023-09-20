6fd174a650a10b8648649bb44b25dc9aca4e9e72
lina-love <lina-ahmed@mail.com>
2023-09-20 16:30:25 +0300

    End of section 9

--- Starter files/EmployeeManagement/ActionFilters/CheckShowStatisticsHeader.cs
+++ Starter files/EmployeeManagement/ActionFilters/CheckShowStatisticsHeader.cs
@@ -1,32 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
-namespace EmployeeManagement.ActionFilters
+namespace EmployeeManagement.ActionFilters;
+public class CheckShowStatisticsHeader : ActionFilterAttribute
 {
-    public class CheckShowStatisticsHeader : ActionFilterAttribute
-    { 
-        public override void OnActionExecuting(ActionExecutingContext context)
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        // if the ShowStatistics header is missing or set to false, 
+        // a BadRequest must be returned.
+        if (!context.HttpContext.Request.Headers.ContainsKey("ShowStatistics"))
         {
-            // if the ShowStatistics header is missing or set to false, 
-            // a BadRequest must be returned.
-                 if (!context.HttpContext.Request.Headers.ContainsKey("ShowStatistics"))
-            {
-                context.Result = new BadRequestResult();
-            }
+            context.Result = new BadRequestResult();
+        }
 
-            // get the ShowStatistics header 
-            if (!bool.TryParse(
-                    context.HttpContext.Request.Headers["ShowStatistics"].ToString(), 
-                    out bool showStatisticsValue))
-            {
-                context.Result = new BadRequestResult();
-            }
+        // get the ShowStatistics header 
+        if (!bool.TryParse(
+                context.HttpContext.Request.Headers["ShowStatistics"].ToString(),
+                out bool showStatisticsValue))
+        {
+            context.Result = new BadRequestResult();
+        }
 
-            // check the value
-            if (!showStatisticsValue)
-            {
-                context.Result = new BadRequestResult();
-            }
-        } 
+        // check the value
+        if (!showStatisticsValue)
+        {
+            context.Result = new BadRequestResult();
+        }
     }
-} 
+}
---
+++ Starter files/EmployeeManagment.Test/ServiceCollectionTests.cs
@@ -0,0 +1,33 @@
+﻿using EmployeeManagement;
+using EmployeeManagement.DataAccess.Services;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmployeeManagment.Test;
+public class ServiceCollectionTests
+{
+    [Fact]
+    public void RegisterDataServices_Execute_DataServicesAreRegistseted()
+    {
+        // Arrange
+        var serviceCollection = new ServiceCollection();
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(
+                new Dictionary<string, string>
+                {
+                    {
+                        "ConnectionStrings:EmployeeManagementDB","AnyValueWillDo"
+                    }
+                })
+            .Build();
+
+        // Act
+        serviceCollection.RegisterDataServices(configuration);
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        // Assert
+        Assert.NotNull(
+            serviceProvider.GetService<IEmployeeManagementRepository>());
+    }
+}
